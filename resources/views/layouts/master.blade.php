5332297fe9991ed3aabb9ed43d203fd2a73c05a8
trungnh <trungnh@solazu.com>
2017-03-24 13:12:43 +0700

    cho tuan

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -48,9 +48,6 @@
                         <div class="social-icons pull-right">
                             <ul class="nav navbar-nav">
                                 <li><a href=""><i class="fa fa-facebook"></i></a></li>
-                                <li><a href=""><i class="fa fa-twitter"></i></a></li>
-                                <li><a href=""><i class="fa fa-linkedin"></i></a></li>
-                                <li><a href=""><i class="fa fa-dribbble"></i></a></li>
                                 <li><a href=""><i class="fa fa-google-plus"></i></a></li>
                             </ul>
                         </div>
@@ -96,7 +93,7 @@
                                 <li><a href=""><i class="fa fa-star"></i> Wishlist</a></li>
                                 <li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>
                                 <li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Cart</a></li>
-                                <li><a class="btn big-login" data-toggle="modal" href="javascript:void(0)" onclick="openLoginModal();">Log in</a></li>
+                                <li><a class="btn big-login" data-toggle="modal" href="javascript:void(0)" onclick="openLoginModal();"><i class="fa fa-sign-in" aria-hidden="true"></i>Đăng nhập</a></li>
                             </ul>
                         </div>
                     </div>
--- resources/views/register/signup.blade.php
+++ resources/views/register/signup.blade.php
@@ -3,15 +3,12 @@
         <div class="modal-content">
            <div class="modal-header">
               <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-              <h4 class="modal-title">Login with</h4>
+              <h4 class="modal-title">Đăng nhập với</h4>
           </div>
           <div class="modal-body">  
               <div class="box">
                    <div class="content">
                       <div class="social">
-                          <a class="circle github" href="/auth/github">
-                              <i class="fa fa-github fa-fw"></i>
-                          </a>
                           <a id="google_login" class="circle google" href="/auth/google_oauth2">
                               <i class="fa fa-google-plus fa-fw"></i>
                           </a>
@@ -21,15 +18,15 @@
                       </div>
                       <div class="division">
                           <div class="line l"></div>
-                            <span>or</span>
+                            <span>Hoặc</span>
                           <div class="line r"></div>
                       </div>
                       <div class="error"></div>
                       <div class="form loginBox">
                           <form method="post" action="/login" accept-charset="UTF-8">
                           <input id="email" class="form-control" type="text" placeholder="Email" name="email">
-                          <input id="password" class="form-control" type="password" placeholder="Password" name="password">
-                          <input class="btn btn-default btn-login" type="button" value="Login" onclick="loginAjax()">
+                          <input id="password" class="form-control" type="password" placeholder="Mật Khẩu" name="password">
+                          <input class="btn btn-default btn-login" type="button" value="Đăng nhập" onclick="loginAjax()">
                           </form>
                       </div>
                    </div>
@@ -38,10 +35,10 @@
                   <div class="content registerBox" style="display:none;">
                    <div class="form">
                       <form method="post" html="{:multipart=>true}" data-remote="true" action="/register" accept-charset="UTF-8">
-                      <input id="email" class="form-control" type="text" placeholder="Email" name="email">
-                      <input id="password" class="form-control" type="password" placeholder="Password" name="password">
-                      <input id="password_confirmation" class="form-control" type="password" placeholder="Repeat Password" name="password_confirmation">
-                      <input class="btn btn-default btn-register" type="submit" value="Create account" name="commit">
+                        <input id="email" class="form-control" type="text" placeholder="Email" name="email">
+                        <input id="password" class="form-control" type="password" placeholder="Mật Khẩu" name="password">
+                        <input id="password_confirmation" class="form-control" type="password" placeholder="Lặp Lại Mật Khẩu" name="password_confirmation">
+                        <input class="btn btn-default btn-register" type="submit" value="Tạo tài khoản" name="commit">
                       </form>
                       </div>
                   </div>
@@ -49,13 +46,13 @@
           </div>
           <div class="modal-footer">
               <div class="forgot login-footer">
-                  <span>Looking to 
-                       <a href="javascript: showRegisterForm();">create an account</a>
-                  ?</span>
+                  <span>Chú ý
+                       <a href="javascript: showRegisterForm();">Tạo tài khoản mới</a>
+                  </span>
               </div>
               <div class="forgot register-footer" style="display:none">
-                   <span>Already have an account?</span>
-                   <a href="javascript: showLoginForm();">Login</a>
+                   <span>Bận đã có tài khoản chưa?</span>
+                   <a href="javascript: showLoginForm();">Đăng nhập</a>
               </div>
           </div>        
         </div>
