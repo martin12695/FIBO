cb9b38cbc84b34af03da782943ea1bd7180ac54b
tuanhm <tuanhm@solazu.com>
2017-03-31 16:43:21 +0700

    update

--- resources/views/user_profile.blade.php
+++ resources/views/user_profile.blade.php
@@ -13,18 +13,15 @@
                                 <h4 class="panel-title">
                                     <a data-toggle="collapse" data-parent="#accordian" href="#sportswear">
                                         <span class="badge pull-right"><i class="fa fa-plus"></i></span>
-                                        Sportswear
+                                        Kết bạn
                                     </a>
                                 </h4>
                             </div>
                             <div id="sportswear" class="panel-collapse collapse">
                                 <div class="panel-body">
                                     <ul>
-                                        <li><a href="">Nike </a></li>
-                                        <li><a href="">Under Armour </a></li>
-                                        <li><a href="">Adidas </a></li>
-                                        <li><a href="">Puma</a></li>
-                                        <li><a href="">ASICS </a></li>
+                                        <li><a href="">Kết bạn bốn phương </a></li>
+                                        <li><a href="">Chat ẩn danh </a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -34,23 +31,16 @@
                                 <h4 class="panel-title">
                                     <a data-toggle="collapse" data-parent="#accordian" href="#mens">
                                         <span class="badge pull-right"><i class="fa fa-plus"></i></span>
-                                        Mens
+                                        Comfession
                                     </a>
                                 </h4>
                             </div>
                             <div id="mens" class="panel-collapse collapse">
                                 <div class="panel-body">
                                     <ul>
-                                        <li><a href="">Fendi</a></li>
-                                        <li><a href="">Guess</a></li>
-                                        <li><a href="">Valentino</a></li>
-                                        <li><a href="">Dior</a></li>
-                                        <li><a href="">Versace</a></li>
-                                        <li><a href="">Armani</a></li>
-                                        <li><a href="">Prada</a></li>
-                                        <li><a href="">Dolce and Gabbana</a></li>
-                                        <li><a href="">Chanel</a></li>
-                                        <li><a href="">Gucci</a></li>
+                                        <li><a href="">Chuyện nam giới</a></li>
+                                        <li><a href="">Chuyện nữ giới</a></li>
+                                        <li><a href="">Chuyện thường ngày</a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -61,81 +51,20 @@
                                 <h4 class="panel-title">
                                     <a data-toggle="collapse" data-parent="#accordian" href="#womens">
                                         <span class="badge pull-right"><i class="fa fa-plus"></i></span>
-                                        Womens
+                                        Giải trí
                                     </a>
                                 </h4>
                             </div>
                             <div id="womens" class="panel-collapse collapse">
                                 <div class="panel-body">
                                     <ul>
-                                        <li><a href="">Fendi</a></li>
-                                        <li><a href="">Guess</a></li>
-                                        <li><a href="">Valentino</a></li>
-                                        <li><a href="">Dior</a></li>
-                                        <li><a href="">Versace</a></li>
+                                        <li><a href="">Nơi ăn uống</a></li>
+                                        <li><a href="">Nơi vui chơi</a></li>
                                     </ul>
                                 </div>
                             </div>
                         </div>
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h4 class="panel-title"><a href="#">Kids</a></h4>
-                            </div>
-                        </div>
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h4 class="panel-title"><a href="#">Fashion</a></h4>
-                            </div>
-                        </div>
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h4 class="panel-title"><a href="#">Households</a></h4>
-                            </div>
-                        </div>
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h4 class="panel-title"><a href="#">Interiors</a></h4>
-                            </div>
-                        </div>
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h4 class="panel-title"><a href="#">Clothing</a></h4>
-                            </div>
-                        </div>
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h4 class="panel-title"><a href="#">Bags</a></h4>
-                            </div>
-                        </div>
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h4 class="panel-title"><a href="#">Shoes</a></h4>
-                            </div>
-                        </div>
                     </div><!--/category-products-->
-
-                    <div class="brands_products"><!--brands_products-->
-                        <h2>Brands</h2>
-                        <div class="brands-name">
-                            <ul class="nav nav-pills nav-stacked">
-                                <li><a href=""> <span class="pull-right">(50)</span>Acne</a></li>
-                                <li><a href=""> <span class="pull-right">(56)</span>Grüne Erde</a></li>
-                                <li><a href=""> <span class="pull-right">(27)</span>Albiro</a></li>
-                                <li><a href=""> <span class="pull-right">(32)</span>Ronhill</a></li>
-                                <li><a href=""> <span class="pull-right">(5)</span>Oddmolly</a></li>
-                                <li><a href=""> <span class="pull-right">(9)</span>Boudestijn</a></li>
-                                <li><a href=""> <span class="pull-right">(4)</span>Rösch creative culture</a></li>
-                            </ul>
-                        </div>
-                    </div><!--/brands_products-->
-
-                    <div class="price-range"><!--price-range-->
-                        <h2>Price Range</h2>
-                        <div class="well">
-                            <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="600" data-slider-step="5" data-slider-value="[250,450]" id="sl2" ><br />
-                            <b>$ 0</b> <b class="pull-right">$ 600</b>
-                        </div>
-                    </div><!--/price-range-->
 
                     <div class="shipping text-center"><!--shipping-->
                         <img src="/images/home/shipping.jpg" alt="" />
@@ -368,9 +297,9 @@
                                         <div class="single-products">
                                             <div class="productinfo text-center">
                                                 <img src="/images/home/recommend1.jpg" alt="" />
-                                                <h2>$56</h2>
-                                                <p>Easy Polo Black Edition</p>
-                                                <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
+                                                <h2>Nguyễn Thị A</h2>
+                                                <p>21 tuổi</p>
+                                                <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-heart"></i>Kết bạn</button>
                                             </div>
                                         </div>
                                     </div>
@@ -380,9 +309,9 @@
                                         <div class="single-products">
                                             <div class="productinfo text-center">
                                                 <img src="/images/home/recommend2.jpg" alt="" />
-                                                <h2>$56</h2>
-                                                <p>Easy Polo Black Edition</p>
-                                                <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
+                                                <h2>Nguyễn Thị B</h2>
+                                                <p>22 tuổi</p>
+                                                <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-heart"></i>Kết bạn</button>
                                             </div>
                                         </div>
                                     </div>
@@ -391,10 +320,10 @@
                                     <div class="product-image-wrapper">
                                         <div class="single-products">
                                             <div class="productinfo text-center">
-                                                <img src="/images/home/recommend3.jpg" alt="" />
-                                                <h2>$56</h2>
-                                                <p>Easy Polo Black Edition</p>
-                                                <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
+                                                <img src="/images/home/recommend1.jpg" alt="" />
+                                                <h2>Nguyễn Thị A</h2>
+                                                <p>21 tuổi</p>
+                                                <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-heart"></i>Kết bạn</button>
                                             </div>
                                         </div>
                                     </div>
@@ -406,9 +335,9 @@
                                         <div class="single-products">
                                             <div class="productinfo text-center">
                                                 <img src="/images/home/recommend1.jpg" alt="" />
-                                                <h2>$56</h2>
-                                                <p>Easy Polo Black Edition</p>
-                                                <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
+                                                <h2>Nguyễn Thị A</h2>
+                                                <p>21 tuổi</p>
+                                                <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-heart"></i>Kết bạn</button>
                                             </div>
                                         </div>
                                     </div>
@@ -418,9 +347,9 @@
                                         <div class="single-products">
                                             <div class="productinfo text-center">
                                                 <img src="/images/home/recommend2.jpg" alt="" />
-                                                <h2>$56</h2>
-                                                <p>Easy Polo Black Edition</p>
-                                                <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
+                                                <h2>Nguyễn Thị B</h2>
+                                                <p>22 tuổi</p>
+                                                <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-heart"></i>Kết bạn</button>
                                             </div>
                                         </div>
                                     </div>
@@ -429,10 +358,10 @@
                                     <div class="product-image-wrapper">
                                         <div class="single-products">
                                             <div class="productinfo text-center">
-                                                <img src="/images/home/recommend3.jpg" alt="" />
-                                                <h2>$56</h2>
-                                                <p>Easy Polo Black Edition</p>
-                                                <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
+                                                <img src="/images/home/recommend2.jpg" alt="" />
+                                                <h2>Nguyễn Thị B</h2>
+                                                <p>22 tuổi</p>
+                                                <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-heart"></i>Kết bạn</button>
                                             </div>
                                         </div>
                                     </div>
