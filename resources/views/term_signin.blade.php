76d191ac799bf085e9600fe32ebc796d0664776a
Trung <huutrung.mmt@gmail.com>
2017-04-03 00:02:42 +0700

    edit

--- resources/views/term_signin.blade.php
+++ resources/views/term_signin.blade.php
@@ -83,7 +83,6 @@
                                     <dd>
                                         <input type="password" tabindex="2" name="password" class="text" ng-model="pass" required>
                                     </dd>
-
                                     <dt>
                                        <span ng-class="message" style="background: red">
                                         Email hoặc mật khẩu chưa đúng!
