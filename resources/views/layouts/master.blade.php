4fdfcbbcfe2e681bf7d0ea8d59bd075857adf077
tuanhm <tuanhm@solazu.com>
2017-04-19 23:20:43 +0700

    update chat

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,9 +15,9 @@
     <link rel="stylesheet" media="screen" href="https://www.twoo.com/static/839517239821694064891/css/sass/ltr/twoo.invite.css">
     <link rel="stylesheet" media="screen" href="https://www.twoo.com/static/456398502947355001/css/sass/ltr/ui.css">
     <link rel="stylesheet" media="screen" href="{{ asset('css/group-more.p1.css') }}">
-    <link rel="stylesheet" media="screen" href="https://www.twoo.com/static/6188132174502284106364/css/sass/ltr/group-more.p2.css">
+    <link rel="stylesheet" media="screen" href="{{ asset('css/group-more.p2.css') }}">
     <link rel="stylesheet" media="screen" href="https://www.twoo.com/static/37339762993591368798/css/sass/ltr/group-more.p3.css">
-    <link rel="stylesheet" media="screen" href="https://www.twoo.com/static/020154603095955706158/css/sass/ltr/twoo.messages.v3.css">
+    <link rel="stylesheet" media="screen" href="{{ asset('css/message-v3.css') }}">
     <link rel="stylesheet" media="screen" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.css">
     <link rel="stylesheet" href="/css/dropdown.css">
     <link rel="stylesheet" href="/css/btn.css">
