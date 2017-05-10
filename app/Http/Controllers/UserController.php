044c7269b135c72a9b85ffab9ff76dccab5f6a2d
Trung <huutrung.mmt@gmail.com>
2017-05-10 18:07:05 +0700

    ...

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,39 +26,53 @@ class UserController
             $info = DB::table('users')->where('id', session('userId'))->first();
             $sex = DB::table('users')
                 ->join('option_sex', 'sex', '=', 'option_sex.id')
+                ->where('users.id', Auth::id())
                 ->select('users.id', 'option_sex.id','option_sex.value')
                 ->first();
 
             $option_sex = DB::table('option_sex')->get();
 
-            $bodys = DB::table('user_description')
+            $bodies = DB::table('user_description')
                 ->join('option_body', 'body', '=', 'option_body.id')
-                ->where('user_id', Auth::id())
+                ->where('user_description.user_id', Auth::id())
                 ->select('user_description.id', 'option_body.id','option_body.value')
                 ->first();
 
             $finances = DB::table('user_description')
                 ->join('option_finace', 'finance', '=', 'option_finace.id')
-                ->where('user_id', Auth::id())
+                ->where('user_description.user_id', Auth::id())
                 ->select('user_description.id', 'option_finace.id','option_finace.value')
                 ->first();
 
             $findSubs = DB::table('user_description')
                 ->join('option_subject_find', 'subject_find', '=', 'option_subject_find.id')
-                ->where('user_id', Auth::id())
+                ->where('user_description.user_id', Auth::id())
                 ->select('user_description.id', 'option_subject_find.id','option_subject_find.value')
                 ->first();
 
             $findAges = DB::table('user_description')
                 ->join('option_subject_eag', 'subject_eag', '=', 'option_subject_eag.id')
-                ->where('user_id', Auth::id())
+                ->where('user_description.user_id', Auth::id())
                 ->select('user_description.id', 'option_subject_eag.id','option_subject_eag.value')
+                ->first();
+
+            $cities = DB::table('users')
+                ->join('province', 'come_from', '=', 'province.id')
+                ->where('users.id', Auth::id())
+                ->select('users.id', 'province.id','province.name')
+                ->first();
+
+            $schools = DB::table('users')
+                ->join('option_school', 'school', '=', 'option_school.id')
+                ->where('users.id', Auth::id())
+                ->select('users.id', 'option_school.id','option_school.value')
                 ->first();
 
             $findAge = DB::table('option_subject_eag')->get();
             $findSub = DB::table('option_subject_find')->get();
             $finance = DB::table('option_finace')->get();
             $province =  DB::table('province')->get();
+            $school = DB::table('option_school')->get();
             $body = DB::table('option_body')->get();
             $infoDes =  DB::table('user_description')->where('user_id', session('userId'))->first();
             if (empty($infoDes)) {
@@ -85,24 +99,50 @@ class UserController
             return view('term_profile',[
                 'info_basic' => $info,
                 'sex'        => $sex,
-                'bodys'      => $bodys,
+                'bodies'      => $bodies,
                 'finances'   => $finances,
                 'findSubs'   => $findSubs,
                 'findAges'   => $findAges,
+                'cities'     => $cities,
+                'schools'    => $schools,
                 'option_sex' => $option_sex,
                 'infoDes'    => $infoDes,
                 'infoHobby'  => $infoHobby,
                 'findAge'    => $findAge,
                 'findSub'    => $findSub,
-                'finance'     => $finance,
+                'finance'    => $finance,
                 'body'       => $body,
-                'province'   => $province
+                'province'   => $province,
+                'school'     => $school
             ]);
         }
     }
 
     public function updateInfo(Request $request) {
         $info = $request->input();
+
+        if( Input::has('phone') ){
+            DB::table('users')
+                ->where('id', Auth::id())
+                ->update(['phone' => $info['phone']]);
+        }
+        if( Input::has('mssv') ){
+            DB::table('users')
+                ->where('id', Auth::id())
+                ->update(['mssv' => $info['mssv']]);
+        }
+        if( Input::has('province')){
+            DB::table('users')
+                ->where('id', Auth::id())
+                ->update(['come_from' => $info['province']]);
+        }
+
+        if( Input::has('school') ) {
+            DB::table('users')
+                ->where('id', Auth::id())
+                ->update(['school' => $info['school']]);
+        }
+
         DB::insert('INSERT INTO user_hobby (user_id, music, movie, sport, hobby) VALUES(?, ?, ?, ?, ?) 
                    ON DUPLICATE KEY UPDATE music= ?, movie= ?, sport= ?, hobby= ?',
             [session('userId'),$info['music'],
