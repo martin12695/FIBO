e7d5b99effc05c6d3d927758e253a357385c5995
Trung <huutrung.mmt@gmail.com>
2017-05-14 18:16:51 +0700

    dqwd

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,50 +2,14 @@
 @section('title', 'Giới thiệu FiBo')
 @section('custom-css')
     <style>
-        table,
-        th,
-        td {
-            border: 1px solid black;
-            border-collapse: collapse;
-        }
-        #header {
-            height: 50px;
-        }
-
-        .SDT {
-            float: right;
-        }
-
-        .image {
-            width: 80px;
-            height: 80px;
-            margin-top: 20px;
-            margin-left: 10px;
-            float: left;
-        }
-
         .Tab {
             display: flex;
             flex-direction: column;
             align-items: center;
-            margin-left: 350px;
-            margin-right: 350px;
+            margin-left: 300px;
+            margin-right: 300px;
             margin-top: 100px;
             margin-bottom: 200px;
-        }
-        .bang{
-            font-family: Arial;
-        }
-
-        td {
-            margin-left: 20px !important;
-        }
-
-        h1 {
-            text-align: center;
-        }
-        .bang p{
-            margin-left: 15px;
         }
     </style>
 @endsection
@@ -68,7 +32,7 @@
                     <th>I</th>
                     <th align="left" colspan="2">TRONG QUÁ TRÌNH ĐĂNG KÍ VÀ ĐĂNG NHẬP</th>
                 </tr>
-                <tr>
+                <tr class="text--subtle">
                     <th scope="row">1</th>
                     <td>FiBo là gì?</td>
                     <td>
@@ -76,7 +40,7 @@
                         <p>- Các dịch vụ hỗ trợ và chăm sóc cuộc hẹn.</p>
                     </td>
                 </tr>
-                <tr>
+                <tr class="text--subtle">
                     <th scope="row">2</th>
                     <td>Các điều kiện và tiêu chí gia nhập cộng đồng này?</td>
                     <td>
@@ -84,12 +48,12 @@
                         <p>- Trình độ: Đại Học.</p>
                     </td>
                 </tr>
-                <tr>
+                <tr class="text--subtle">
                     <th scope="row">3</th>
                     <td>Đối tượng tham gia?</td>
                     <td>Sinh viên đại học.</td>
                 </tr>
-                <tr>
+                <tr class="text--subtle">
                     <th scope="row">4</th>
                     <td>Khác biệt giữa FiBo với các ứng dụng/ dịch vụ hẹn hò khác?</td>
                     <td>
@@ -97,7 +61,7 @@
                         <p>- Công cụ book lịch hẹn tiền lợi.</p>
                     </td>
                 </tr>
-                <tr>
+                <tr class="text--subtle">
                     <th scope="row">5</th>
                     <td>Phạm vi hoạt động của FiBo?</td>
                     <td>
@@ -105,7 +69,7 @@
                         <p>- Đại học Khoa học xã hội và nhân văn. </p>
                     </td>
                 </tr>
-                <tr>
+                <tr class="text--subtle">
                     <th scope="row">6</th>
                     <td>Làm thế nào để có account để đăng nhập vào trang web?</td>
                     <td>
@@ -116,7 +80,7 @@
                         <p><b>Bước 5:</b> Nhận account và password; nghiên cứu kĩ quy trình và điều khoản dịch vụ để đảm bảo sử dụng đúng mục đích và đúng quy định của cộng đồng.</p>
                     </td>
                 </tr>
-                <tr>
+                <tr class="text--subtle">
                     <th scope="row">7</th>
                     <td>Profile của thành viên trên ứng dụng sẽ hiển thị những thông tin gì?</td>
                     <td>
@@ -130,7 +94,7 @@
                         <p>- Tình trạng hiện tại.</p>
                     </td>
                 </tr>
-                <tr>
+                <tr class="text--subtle">
                     <th scope="row">8</th>
                     <td>Phương pháp thẩm định hồ sơ thành viên?</td>
                     <td>
@@ -140,7 +104,7 @@
                         <p>- Thông qua các giấy tờ, tài liệu có giá trị xác thực của thành viên (Cụ thể là CMND và thẻ sinh viên).</p>
                     </td>
                 </tr>
-                <tr>
+                <tr class="text--subtle">
                     <th scope="row">9</th>
                     <td>Gia nhập FiBo có chắc chắn tìm được người yêu/đối tượng quan tâm ?</td>
                     <td>
@@ -152,14 +116,14 @@
                     <th>II</th>
                     <th align="left" colspan="2">CÁC THẮC MẮC VỀ CHI PHÍ</th>
                 </tr>
-                <tr>
+                <tr class="text--subtle">
                     <th scope="row">10</th>
                     <td>Những chi phí phải trả để tham gia FiBo gồm những khoản gì?</td>
                     <td>
                         <p>Vì mục đích xây dựng một mối quan hệ gắn kế giữa 2 trường đại học nên web sẽ không lấy phí dịch vụ.</p>
                     </td>
                 </tr>
-                <tr>
+                <tr class="text--subtle">
                     <th scope="row">11</th>
                     <td>Có được đăng kí nhiều tài khoản cùng lúc không?</td>
                     <td>
@@ -170,14 +134,14 @@
                     <th>III</th>
                     <th align="left" colspan="2">TRONG QUÁ TRÌNH SỬ DỤNG</th>
                 </tr>
-                <tr>
+                <tr class="text--subtle">
                     <td scope="row">12</td>
                     <td>Trách nhiệm của FiBo khi thành viên gặp rắc rối với đối tượng hẹn hò?</td>
                     <td>
                         <p>FiBo chỉ là dịch vụ trung gian, tạo điều kiện để các thành viên có hồ sơ phù hợp có cơ hội kết nối, giao lưu và gặp gỡ nhau. FiBo không chịu trách nhiệm hay can thiệp vào các vấn đề liên đới trong diễn biến các mối quan hệ sau đó cũng như các hậu quả liên quan có thể xảy ra trong mối quan hệ giữa các thành viên . </p>
                     </td>
                 </tr>
-                <tr>
+                <tr class="text--subtle">
                     <td scope="row">13</td>
                     <td>Các dịch vụ chăm sóc hẹn hò là như thế nào?</td>
                     <td>
@@ -190,7 +154,7 @@
                         </p>
                     </td>
                 </tr>
-                <tr>
+                <tr class="text--subtle">
                     <td scope="row">14</td>
                     <td>Trong quá trình FiBo nếu gặp vấn đề gì thì liên hệ với ai? (gặp lỗi kĩ thuật trên ứng dụng, cần hỗ trợ, tư vấn, giải đáp thắc mắc…)</td>
                     <td>
@@ -198,21 +162,21 @@
                         <p>- Gửi mail về ducaotien@gmail.com</p>
                     </td>
                 </tr>
-                <tr>
+                <tr class="text--subtle">
                     <td scope="row">15</td>
                     <td>Nếu nhận thấy hoặc phát hiện các thành viên khác có vấn đề thì làm thế nào? (đã kết hôn, có hành vi - ứng xử không phù hợp với tiêu chuẩn của cộng đồng…)</td>
                     <td>
                         <p>Sau khi nhận được report, FiBo sẽ thẩm định và xác minh rồi mới xử lí nên không lo vấn đề bị report sai.</p>
                     </td>
                 </tr>
-                <tr>
+                <tr class="text--subtle">
                     <td scope="row">16</td>
                     <td>Nếu bị thành viên khác report sai thì làm thế nào?</td>
                     <td>
                         <p>Sau khi nhận được report, FiBo sẽ thẩm định và xác minh rồi mới xử lí nên không lo vấn đề bị report sai.</p>
                     </td>
                 </tr>
-                <tr>
+                <tr class="text--subtle">
                     <td scope="row">17</td>
                     <td>Cung cấp thông tin liên hệ và thông tin cá nhân cho FiBo có sợ bị tiết lộ cho bên thứ 3 không?</td>
                     <td>
